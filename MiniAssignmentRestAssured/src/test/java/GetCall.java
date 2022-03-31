cc352549ef4b276ccc3a1e335d76b500f28bd271
udayprsingh <udayprsingh@deloitte.com>
2022-03-30 23:26:21 -0700

    method name get added

--- MiniAssignmentRestAssured/src/test/java/GetCall.java
+++ MiniAssignmentRestAssured/src/test/java/GetCall.java
@@ -1,9 +1,8 @@
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
-public class Solution1 {
+public class GetCall {
     @Test
     public void get_call(){
         given().
